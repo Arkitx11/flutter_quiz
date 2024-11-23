97b83b74c8ed292d3b5bc82b4345ccd911f5ac84
Archit Kumar <arkit.x11@gmail.com>
2024-11-23 17:32:03 +0530

    feat: drop support for linux and web

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
-  await dotenv.load(fileName: "api.env");
+  await dotenv.load(fileName: "env/api.env");
 
   await Supabase.initialize(
     url: dotenv.env['SUPABASE_URL']!,
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,10 +1,7 @@
-import 'dart:io';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-
 
 class Authentication {
   final supabaseInstance = Supabase.instance;
@@ -13,7 +10,6 @@ class Authentication {
 
   Future<void> signInAnonymously() async {
     try {
-
       await supabaseInstance.client.auth.signInAnonymously();
     } catch (error) {
       print('Error signing in anonymously: $error');
@@ -21,12 +17,11 @@ class Authentication {
   }
 
   Future<void> nativeGoogleSignIn() async {
-    await dotenv.load(fileName: "clientId.env");
-    var webClientId = dotenv.env['WEB_CLIENT_ID'];
+    await dotenv.load(fileName: "env/clientId.env");
+    var webClientId = dotenv.env['WEBCLIENT_ID'];
 
-    final GoogleSignIn googleSignIn = GoogleSignIn(
-      serverClientId: webClientId,
-    );
+
+    final GoogleSignIn googleSignIn = GoogleSignIn(clientId: webClientId!);
     final googleUser = await googleSignIn.signIn();
     final googleAuth = await googleUser!.authentication;
     final accessToken = googleAuth.accessToken;
@@ -39,24 +34,11 @@ class Authentication {
       throw 'No ID Token found.';
     }
 
-    if (Platform.isAndroid) {
-      await supabaseInstance.client.auth.signInWithIdToken(
-        provider: OAuthProvider.google,
-        idToken: idToken,
-        accessToken: accessToken,
-      );
-    }
-    if (!Platform.isAndroid || !Platform.isIOS) {
-      await supabaseInstance.client.auth.signInWithOAuth(
-        OAuthProvider.google,
-        redirectTo: kIsWeb ? null : 'my.scheme://my-host', // Optionally set the redirect link to bring back the user via deeplink.
-        authScreenLaunchMode:
-        kIsWeb ? LaunchMode.platformDefault : LaunchMode.externalApplication, // Launch the auth screen in a new webview on mobile.
-      );
-
-    }
-
-
+    await supabaseInstance.client.auth.signInWithIdToken(
+      provider: OAuthProvider.google,
+      idToken: idToken,
+      accessToken: accessToken,
+    );
   }
 
   Future<void> signOut() async {
